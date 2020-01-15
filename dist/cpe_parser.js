8d54c25f7cc8f37181bb9f77a52c0409617902e6
Jonathan Fasking <jonfasking@gmail.com>
2020-01-14 19:45:02 -0600

    updated dist

--- dist/cpe_parser.js
+++ dist/cpe_parser.js
@@ -2,6 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const cpe_1 = require("./cpe");
 class CpeParser {
+    constructor() {
+        this._uriBindingPrefix = 'cpe:/';
+        this._formattedBindingPrefix = 'cpe:2.3:';
+        this._uriBindingExtendedAttributesDelimiterKey = ':~';
+    }
     /**
      * Parse a cpe 23 string value.
      *
@@ -12,9 +17,9 @@ class CpeParser {
     parseCpe23(cpe) {
         if (typeof cpe !== 'string' || !cpe)
             return null;
-        const prefix = 'cpe:2.3:';
-        let substring = cpe.startsWith(prefix) ? cpe.substring(prefix.length) : cpe;
-        let attributes = substring.split(':');
+        let trimmedCpe = cpe.trim();
+        let substring = this.getCpeAttributesSubstring(trimmedCpe);
+        let attributes = this.getAttributeList(trimmedCpe, substring);
         let cpeModel = new cpe_1.Cpe();
         for (let i = 0; i < attributes.length; i++) {
             let parsedAttributeValue = this.parseAttributeValue(attributes[i]);
@@ -56,6 +61,49 @@ class CpeParser {
         return cpeModel;
     }
     /**
+     * Get an attribute list from either a uri binding
+     * or formatted binding cpe value. The cpeAttributes
+     * need to be pristine of any binding prefix.
+     *
+     * @private
+     * @param {string} fullCpe
+     * @param {string} cpeAttributes
+     * @returns {string[]}
+     * @memberof CpeParser
+     */
+    getAttributeList(fullCpe, cpeAttributes) {
+        let attributes = cpeAttributes.split(':');
+        if (!this.hasUriBinding(fullCpe) || !fullCpe.includes(this._uriBindingExtendedAttributesDelimiterKey))
+            return attributes;
+        let extendedAttributes = attributes.filter(x => x.startsWith('~')).pop();
+        let asteriskFilled = extendedAttributes.split('~').map(x => x || '*');
+        attributes.pop();
+        attributes = attributes.concat(asteriskFilled);
+        return attributes;
+    }
+    /**
+     * Check if the cpe value has a uri binding syntax. This is
+     * done by checking if the value starts with `cpe:/`.
+     *
+     * @param {string} cpe
+     * @returns {boolean}
+     * @memberof CpeParser
+     */
+    hasUriBinding(cpe) {
+        return cpe && typeof cpe === 'string' && cpe.startsWith(this._uriBindingPrefix);
+    }
+    /**
+     * Check if the cpe value has a formatted binding syntax. This is
+     * done by checking if the value starts with `cpe:2.3:`.
+     *
+     * @param {string} cpe
+     * @returns {boolean}
+     * @memberof CpeParser
+     */
+    hasFormattedBinding(cpe) {
+        return cpe && typeof cpe === 'string' && cpe.startsWith(this._formattedBindingPrefix);
+    }
+    /**
      * Applies logic per section 5.3.2 of:
      * https://nvlpubs.nist.gov/nistpubs/Legacy/IR/nistir7695.pdf
      *
@@ -69,6 +117,22 @@ class CpeParser {
         if (typeof rawAttributeValue !== 'string' || !rawAttributeValue)
             return '';
         return rawAttributeValue.trim().replace(/_/g, ' ');
+    }
+    /**
+     * Get the substring of the cpe that contain the values
+     * that can be parsed into the hydrated model.
+     *
+     * @private
+     * @param {string} fullCpe
+     * @returns {string}
+     * @memberof CpeParser
+     */
+    getCpeAttributesSubstring(fullCpe) {
+        if (this.hasFormattedBinding(fullCpe))
+            return fullCpe.substring(this._formattedBindingPrefix.length);
+        if (this.hasUriBinding(fullCpe))
+            return fullCpe.substring(this._uriBindingPrefix.length);
+        return fullCpe;
     }
 }
 exports.CpeParser = CpeParser;
